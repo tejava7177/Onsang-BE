7f2f6598d125a335f9b2c0c96fa5c07f70706f58
Ogu1208 <kdasunb@naver.com>
2023-06-27 20:45:56 +0900

    [feat] : add Counselor repository

---
+++ src/main/java/com/onsang/onsangbe/repository/CounselorRepository.java
@@ -0,0 +1,7 @@
+package com.onsang.onsangbe.repository;
+
+import com.onsang.onsangbe.domain.Counselor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CounselorRepository extends JpaRepository <Counselor, Long> {
+}
