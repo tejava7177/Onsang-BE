079e69f2f032efb08eba4a2550a0f54b6057e6d6
Ogu1208 <kdasunb@naver.com>
2023-06-27 19:16:16 +0900

    first commit

---
+++ src/main/java/com/onsang/onsangbe/OnsangBeApplication.java
@@ -0,0 +1,13 @@
+package com.onsang.onsangbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsangBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnsangBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onsang/onsangbe/OnsangBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onsang.onsangbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsangBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
